7e09ad94eb4a7b3dd637960cc55b816b8dadbde2
Owl of Avery <owlofavery@gmail.com>
2024-05-01 21:43:41 -0400

    Add Pinia

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { GesturePlugin } from '@vueuse/gesture'
 import './style.scss'
 import App from './App.vue'
 
+const pinia = createPinia()
 const app = createApp(App)
 
+app.use(pinia)
 app.use(GesturePlugin)
 app.mount('#app')
